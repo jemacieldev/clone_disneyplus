2f1d4bc75ab446d3214daf3afd6c7214053d69ed
Je <jemacieldev@gmail.com>
2024-07-16 23:30:49 -0300

    att

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,35 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const fs = require('fs');
 
-function styles() {
+async function styles() {
     return gulp.src('./src/styles/*.scss')
        .pipe(sass({ outputStyle: 'compressed' }))
        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+async function images() {
+    const imagemin = (await import('gulp-imagemin')).default;
+    if (fs.existsSync('./src/images')) {
+        const files = await new Promise((resolve, reject) => {
+            gulp.src('./src/images/**/*')
+                .pipe(imagemin())
+                .pipe(gulp.dest('./dist/images'))
+                .on('end', resolve)
+                .on('error', reject);
+        });
+        console.log('Images processed.');
+    } else {
+        console.log('No images directory found, skipping images task.');
+    }
 }
+
+exports.default = gulp.parallel(styles, images);
+
+exports.watch = function() {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+}
+
+
+
+
